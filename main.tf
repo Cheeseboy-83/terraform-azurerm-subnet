52bb53d2148c20b1729eb186facae9af42794d3a
Aaron Curtis <aaron.curtis@gatesfoundation.org>
2024-10-02 10:09:17 -0700

    fix: adjust main.tf ref to private link service policies

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ resource "azurerm_subnet" "snet" {
   virtual_network_name = var.virtual_network_name
   address_prefixes     = var.address_prefixes
 
-  default_outbound_access_enabled       = var.default_outbound_access_enabled
-  private_endpoint_network_policies     = var.private_endpoint_network_policies
-  private_link_service_network_policies = var.private_link_service_network_policies
+  default_outbound_access_enabled               = var.default_outbound_access_enabled
+  private_endpoint_network_policies             = var.private_endpoint_network_policies
+  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
 
   service_endpoints           = var.service_endpoints
   service_endpoint_policy_ids = var.service_endpoint_policy_ids
