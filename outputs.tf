0754e71495eac88698f5d37b50019015a54296ba
Aaron Curtis <aaron.curtis@gatesfoundation.org>
2024-10-02 11:43:40 -0700

    fix: add pe network policies output to debug

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "id" {
   description = "The ID of the subnet"
   value       = azurerm_subnet.snet.id
 }
+
+output "private_endpoint_network_policies" {
+  description = "The network policies for private endpoints on the subnet"
+  value       = azurerm_subnet.snet.private_endpoint_network_policies
+}
